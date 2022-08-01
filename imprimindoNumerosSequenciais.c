b781924c0c0969cfece2571aecf52c2048145545
JackcielF <jackcielfelix555@gmail.com>
2022-08-01 18:42:23 -0300

    Imprimindo numeros

---
+++ imprimindoNumerosSequenciais.c
@@ -0,0 +1,12 @@
+/* Faça um programa que imprime os números de 0 a
+   1000 em ordem crescente(menor para o maior) de 10
+   em 10 */
+// LEVEL: Simple
+
+#include <stdio.h>
+int main(){
+  for(int i = 0;i <= 1000;i+=10){
+    printf("%d\n", i);
+  }
+}
+
